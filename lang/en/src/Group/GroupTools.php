0d9d2e0b86642c1309d74ab2306307711fc7701e
Nik Samokhvalov <nik@samokhvalov.info>
2015-10-11 19:44:57 +0300

    Localization

---
+++ lang/en/src/Group/GroupTools.php
@@ -0,0 +1,4 @@
+<?php
+
+$MESS['BEX_TOOLS_GROUP_EMPTY_STRING_ID'] = 'Enter the string id.';
+$MESS['BEX_TOOLS_GROUP_STRING_ID_ALREDY_USED'] = 'This string id already used in other group.';
---
+++ lang/ru/src/Group/GroupTools.php
@@ -0,0 +1,4 @@
+<?php
+
+$MESS['BEX_TOOLS_GROUP_EMPTY_STRING_ID'] = 'Введите символьный идентификатор.';
+$MESS['BEX_TOOLS_GROUP_STRING_ID_ALREDY_USED'] = 'Группа с таким символьным идентификатором уже существует.';
