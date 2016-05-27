09fe2a4f1ed588cd7d67012d661a1edc51b5f7c8
Nik Samokhvalov <nik@samokhvalov.info>
2016-05-27 20:38:54 +0300

    Loader

---
+++ src/Loader.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Bex\Tools;
+
+use Bitrix\Main\EventManager;
+
+/**
+ * Bootstrap of the Bex\Tools.
+ * 
+ * @author Nik Samokhvalov <nik@samokhvalov.info>
+ */
+class Loader
+{
+    /**
+     * @var EventManager
+     */
+    protected $eventManager;
+    /**
+     * @var array Handlers of the Bex\Tools.
+     */
+    private $handlers = [
+        ['main', 'OnBeforeGroupAdd', ['\Bex\Tools\Group\GroupTools', 'onBeforeGroupAdd']],
+        ['main', 'OnBeforeGroupUpdate', ['\Bex\Tools\Group\GroupTools', 'onBeforeGroupUpdate']],
+        ['main', 'OnAfterGroupAdd', ['\Bex\Tools\Group\GroupFinder', 'onAfterGroupAdd']],
+        ['main', 'OnAfterGroupUpdate', ['\Bex\Tools\Group\GroupFinder', 'onAfterGroupUpdate']],
+        ['main', 'OnGroupDelete', ['\Bex\Tools\Group\GroupFinder', 'onGroupDelete']],
+        ['iblock', 'OnBeforeIBlockAdd', ['\Bex\Tools\Iblock\IblockTools', 'onBeforeIBlockAdd']],
+        ['iblock', 'OnBeforeIBlockUpdate', ['\Bex\Tools\Iblock\IblockTools', 'onBeforeIBlockUpdate']],
+        ['iblock', 'OnAfterIBlockAdd', ['\Bex\Tools\Iblock\IblockFinder', 'onAfterIBlockAdd']],
+        ['iblock', 'OnAfterIBlockUpdate', ['\Bex\Tools\Iblock\IblockFinder', 'onAfterIBlockUpdate']],
+        ['iblock', 'OnIBlockDelete', ['\Bex\Tools\Iblock\IblockFinder', 'onIBlockDelete']],
+        ['highloadblock', '\Bitrix\Highloadblock\HighloadBlock::OnAdd', ['\Bex\Tools\HlBlock\HlBlockFinder', 'onAfterSomething']],
+        ['highloadblock', '\Bitrix\Highloadblock\HighloadBlock::OnAfterUpdate', ['\Bex\Tools\HlBlock\HlBlockFinder', 'onAfterSomething']],
+        ['highloadblock', '\Bitrix\Highloadblock\HighloadBlock::OnAfterDelete', ['\Bex\Tools\HlBlock\HlBlockFinder', 'onAfterSomething']]
+    ];
+    
+    public function __construct()
+    {
+        $this->eventManager = EventManager::getInstance();
+    }
+
+    /**
+     * Runs loader.
+     */
+    public function run()
+    {
+        $this->registerHandlers();
+    }
+    
+    /**
+     * Register handlers of the Bitrix events.
+     */
+    protected function registerHandlers()
+    {
+        foreach ($this->getHandlers() as $handler) {
+            $this->eventManager->addEventHandler($handler[0], $handler[1], [$handler[2], $handler[3]]);
+        }
+    }
+
+    /**
+     * Gets event handlers of the Bex\Tools.
+     * 
+     * @return array
+     */
+    public function getHandlers()
+    {
+        return (array) $this->handlers;
+    }
+}
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -8,28 +8,5 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     return false;
 }
 
-$manager = \Bitrix\Main\EventManager::getInstance();
-
-$manager->addEventHandler('main', 'OnBeforeGroupAdd', ['\Bex\Tools\Group\GroupTools', 'onBeforeGroupAdd']);
-$manager->addEventHandler('main', 'OnBeforeGroupUpdate', ['\Bex\Tools\Group\GroupTools', 'onBeforeGroupUpdate']);
-
-$manager->addEventHandler('main', 'OnAfterGroupAdd', ['\Bex\Tools\Group\GroupFinder', 'onAfterGroupAdd']);
-$manager->addEventHandler('main', 'OnAfterGroupUpdate', ['\Bex\Tools\Group\GroupFinder', 'onAfterGroupUpdate']);
-$manager->addEventHandler('main', 'OnGroupDelete', ['\Bex\Tools\Group\GroupFinder', 'onGroupDelete']);
-
-$manager->addEventHandler('iblock', 'OnBeforeIBlockAdd', ['\Bex\Tools\Iblock\IblockTools', 'onBeforeIBlockAdd']);
-$manager->addEventHandler('iblock', 'OnBeforeIBlockUpdate', ['\Bex\Tools\Iblock\IblockTools', 'onBeforeIBlockUpdate']);
-
-$manager->addEventHandler('iblock', 'OnAfterIBlockAdd', ['\Bex\Tools\Iblock\IblockFinder', 'onAfterIBlockAdd']);
-$manager->addEventHandler('iblock', 'OnAfterIBlockUpdate', ['\Bex\Tools\Iblock\IblockFinder', 'onAfterIBlockUpdate']);
-$manager->addEventHandler('iblock', 'OnIBlockDelete', ['\Bex\Tools\Iblock\IblockFinder', 'onIBlockDelete']);
-
-$manager->addEventHandler('highloadblock', '\Bitrix\Highloadblock\HighloadBlock::OnAdd',
-    ['\Bex\Tools\HlBlock\HlBlockFinder', 'onAfterSomething']
-);
-$manager->addEventHandler('highloadblock', '\Bitrix\Highloadblock\HighloadBlock::OnAfterUpdate',
-    ['\Bex\Tools\HlBlock\HlBlockFinder', 'onAfterSomething']
-);
-$manager->addEventHandler('highloadblock', '\Bitrix\Highloadblock\HighloadBlock::OnAfterDelete',
-    ['\Bex\Tools\HlBlock\HlBlockFinder', 'onAfterSomething']
-);
+$manager = new \Bex\Tools\Loader();
+$manager->run();
